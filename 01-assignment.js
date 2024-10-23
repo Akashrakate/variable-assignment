3ff4dc53f4a9d014847d4db7719bca98d0254366
Akashrakate <akashrakate7137@gmail.com>
2024-10-23 12:35:35 +0530

    variable assignment first js
    first assignments variable js

---
+++ 01-assignment.js
@@ -0,0 +1,20 @@
+
+city="Sangli";
+console.log("City :",city );
+
+friend="Yogesh";
+console.log("Best friend :" ,friend);
+
+country="India";
+console.log("Country :" ,country);
+
+school="Shivaji High School";
+console.log("School Name:",school);
+
+college="NMPI College";
+console.log("College Name :" ,college);
+
+
+
+
+
---
+++ 02-assigment.js
@@ -0,0 +1,43 @@
+
+fullName = "Akash Bhagawan Rakate";
+console.log("Full name: ", fullName);
+
+Name = "Akash Rakate";
+console.log("First & Last Name: ", Name);
+
+Name = "Akash";
+console.log("First Name: ",Name);
+
+Name = "Bhagawan";
+console.log("Middle Name: ", Name);
+
+Name = "Rakate";
+console.log("Last name: ",Name);
+
+language = "I Love Only JS";
+console.log("Language: ", language);
+
+Name = "Akash";
+console.log("First Name: ",Name);
+
+Name = "Rakate";
+console.log("Last name: ",Name);
+
+age= "23";
+console.log("Age:" ,age);
+
+address="Sangli,Walwa";
+console.log("Address:",address);
+
+pincode="416301";
+console.log("Pin-Code:", pincode);
+
+gender="Male";
+console.log("Gender:", gender);
+
+ismarried="False";
+console.log("Is-Married:",ismarried);
+
+
+
+
